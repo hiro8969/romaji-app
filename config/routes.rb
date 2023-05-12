a1d2c3a6411ca3e6bf0515eba7fe4dfd12a22de5
hiro8969 <129718390+hiro8969@users.noreply.github.com>
2023-05-12 15:50:55 +0900

    コントローラー追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'hiraganas#index'
-  get '/hiraganas/:id', to: 'hiraganas#show
-end
+   root to: "hiraganas#index"
+   get '/hiraganas/:id', to: 'hiraganas#show'
+
+   resources :hiraganas, only: [:index, :show]
+  end
